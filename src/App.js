1a5daed6400b7559aae4bdf486f0b6660361f393
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-05 16:36:55 +0300

    adding the nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AddTicketForm from './Components/Helpdesk/AddTicketForm';
+import Navbar from './Components/NavBar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>testing</h1>
+      <Navbar/>
+      <AddTicketForm/>
     </div>
   );
 }
