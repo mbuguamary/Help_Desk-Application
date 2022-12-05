479ed12ab58195cdb8441e0090ce9a6bc433be9e
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-05 16:37:45 +0300

    created category form

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Button, Form,Input } from "antd";
 
 const Category = () => {
+
   return (
-    <div>Category</div>
+    <div>    
+    <Form form={form} onFinish={handleSubmitForm} onSubmit={handleSubmitForm}> 
+    <Form.Item name='category'>
+       <input></input>
+    </Form.Item>
+    <Form.Item>
+       <Button type="primary" htmlType="submit">Submit</Button>
+    </Form.Item>
+ </Form>
+ </div>
   )
 }
 
