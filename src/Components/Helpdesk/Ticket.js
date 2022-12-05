5e5b6261f0330bd0dbc2891efa8e32fda46baad1
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-05 16:38:27 +0300

    created the table

--- src/Components/Helpdesk/Ticket.js
+++ src/Components/Helpdesk/Ticket.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Ticket = () => {
   return (
-    <div>Ticket</div>
+    <table >
+    <tbody>
+      { <tr >
+        <th >
+          <h3 className="ui center aligned header">Id</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Description</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Creator</h3>
+
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Category</h3>
+          
+        </th>
+        <th>
+          <h3 className="ui center aligned header">status</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">created</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Updated</h3>
+        </th>
+      </tr> }
+      </tbody>
+      </table>
+
   )
 }
 
