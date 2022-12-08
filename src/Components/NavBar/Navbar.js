c4091bb8f36947d764f2fe080d8e049b5a2ea5a5
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-08 10:34:27 +0300

    renamed the file

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,79 +1,99 @@
-import React from 'react'
 import { LaptopOutlined } from '@ant-design/icons';
 import {  Layout, Menu } from 'antd';
+import React from 'react';
 import './Navbar.css'
 import "antd/dist/antd.min.css"
 import { Link, NavLink, Outlet } from 'react-router-dom';
 
 const { Header, Content, Sider } = Layout;
-const Navbar = () => {
-  <Layout>
-  <Header className="header" >
-    <div className="logo"  />
-    <Menu theme="dark" mode="horizontal" />
-    {/* <NavLink    style={{
-        padding: '0 24px 24px', float: 'Left'
-      }} >Sacco Web App</NavLink>
-    <NavLink to="/AddLoanForm" style={{
-        padding: '0 24px 24px',
-      }}>Home</NavLink>
-    <NavLink to="/Notes" style={{
-        padding: '0 24px 24px',
-      }}>Notes</NavLink>
-    <NavLink to="/Questions" style={{
-        padding: '0 24px 24px',
-      }}>Questioners</NavLink>
-    <NavLink to="/Login" style={{
-        padding: '0 24px 24px', float: 'right'
-      }}>Login</NavLink> */}
-  </Header>
-  <Layout>
-    <Sider width={200} className="site-layout-background">
-      <Menu
-        mode="inline"
-        //defaultSelectedKeys={['1']}
-        //defaultOpenKeys={['sub1']}
-        style={{
-          height: '100%',
-          borderRight: 0,
-        }}
-      
-        
-      >
 
-       
-        <Menu.Item>
-         <Link > Register Loan </Link>
-         </Menu.Item>
-         <Menu.Item>
-         <Link > Applied Loan List</Link>
-        </Menu.Item>
+
+const Navbar = () => (
+  <Layout>
+    <Header className="header" >
+      <div className="logo"  />
+      <Menu theme="dark" mode="horizontal" />
+      <NavLink    style={{
+          padding: '0 24px 24px', float: 'Left'
+        }} >Metro Help Desk</NavLink>
+      <NavLink to="/AddTicketForm" style={{
+          padding: '0 24px 24px',
+        }}>Home</NavLink>
+      
+      
+      <NavLink to="/Login" style={{
+          padding: '0 24px 24px', float: 'right'
+        }}>Login</NavLink>
+    </Header>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu 
+          mode="inline"
+          //defaultSelectedKeys={['1']}
+          //defaultOpenKeys={['sub1']}
+          style={{
+            height: '100%',
+            borderRight: 0,
+          }}
+        
+          
+        >
+          
+          <Menu style={{ padding: '0'}}>Settings
          
-
+          <Menu.Item> 
+           <Link to="/Category"> Register Category </Link>
+           </Menu.Item>
+           <Menu.Item>
+           <Link to="/Department"> Register Department</Link>
+          </Menu.Item>
           </Menu>
-    </Sider>
-    <Layout
-      style={{
-        padding: '0 24px 24px',
-      }}
-    >
-      
-      <Content
-        className="site-layout-background"
+          <Menu>Tickets
+          <Menu.Item> 
+           <Link to="/AddTicketForm"> Raise Ticket </Link>
+           </Menu.Item>
+           <Menu.Item> 
+           <Link to="/TicketList">View Raised Tickets </Link>
+           </Menu.Item>
+          </Menu>
+          <Menu>Reports
+          <Menu.Item> 
+          <Link to="/DepartmentList">Department list </Link>
+           </Menu.Item>
+           <Menu.Item> 
+           <Link to="/CategoryList">Category List </Link>
+           </Menu.Item>
+           <Menu.Item> 
+           <Link to="/TicketList">Resolved Tickets </Link>
+           </Menu.Item>
+            </Menu> 
+          </Menu>
+          
+
+          
+            
+      </Sider>
+      <Layout
         style={{
-          padding: 24,
-          margin: 0,
-          minHeight: 280,
+          padding: '0 24px 24px',
         }}
       >
         
-        <Outlet/>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          
+          <Outlet/>
 
-      </Content>
+        </Content>
+      </Layout>
     </Layout>
   </Layout>
-</Layout>
-  
-}
+);
 
-export default Navbar
+export default Navbar;
