0400a045a0cc7169f3fc5d7f0f263b2de8f00c60
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-08 10:37:55 +0300

    added ticket list table

--- src/Components/Helpdesk/Ticket.js
+++ src/Components/Helpdesk/Ticket.js
@@ -1,36 +1,16 @@
 import React from 'react'
 
-const Ticket = () => {
+const Ticket = ({id,name,summary,priority,status,created_at,updated_at}) => {
   return (
-    <table >
-    <tbody>
-      { <tr >
-        <th >
-          <h3 className="ui center aligned header">Id</h3>
-        </th>
-        <th>
-          <h3 className="ui center aligned header">Description</h3>
-        </th>
-        <th>
-          <h3 className="ui center aligned header">Creator</h3>
-
-        </th>
-        <th>
-          <h3 className="ui center aligned header">Category</h3>
-          
-        </th>
-        <th>
-          <h3 className="ui center aligned header">status</h3>
-        </th>
-        <th>
-          <h3 className="ui center aligned header">created</h3>
-        </th>
-        <th>
-          <h3 className="ui center aligned header">Updated</h3>
-        </th>
-      </tr> }
-      </tbody>
-      </table>
+    <tr>
+      <td>{id}</td>
+      <td>{name}</td>
+      <td>{summary}</td>
+      <td>{priority}</td>
+      <td>{status}</td>
+      <td>{created_at}</td>
+ 
+    </tr>
 
   )
 }
--- src/Components/Helpdesk/TicketList.js
+++ src/Components/Helpdesk/TicketList.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Ticket from './Ticket'; 
+import './/Ticket.css'
+const TicketList = (props) => {
+ const[tickets,SetTickets] = useState([])
+ 
+ useEffect(() =>{
+fetch("http://localhost:3000/tickets")
+.then(res => res.json())
+.then(data => {SetTickets(data)});
+ },[]);
+ 
+  return(
+ <table >
+    <tbody>
+      { <tr >
+        <th >
+          <h3 className="ui center aligned header">Id</h3>
+        </th>
+        
+        <th>
+          <h3 className="ui center aligned header">Creator</h3>
 
-const TicketList = () => {
-  return (
-    <div>TicketList</div>
-  )
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Summary</h3>
+          
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Priority</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Status</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Created At</h3>
+        </th>
+        
+      </tr> }
+      {tickets.map(ticket => {
+        return(
+        <Ticket 
+         key={ticket.id}
+         id={ticket.id}
+         name={ticket.name}
+         summary={ticket.summary}
+         priority={ticket.priority}
+         status={ticket.status}
+         created_at={ticket.created_at}
+        
+        />);
+      })
+
+      }
+      </tbody>
+      </table>
+      )
 }
 
 export default TicketList
