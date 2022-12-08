5122bcd525598d4b96ded98f1d33d0cbdb185515
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-08 10:35:56 +0300

    created categories file

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import { Button, Form,Input } from "antd";
-
+import { useState } from "react";
+const layout = {
+   labelCol: {
+     span: 8,
+   },
+   wrapperCol: {
+     span: 16,
+   },
+ };
 const Category = () => {
+   const [form] = Form.useForm();
+   const [formData,setFormData] = useState({});
+   function handleChangeForm (e){
+      setFormData({...formData, [e.target.name]:[e.target.value]})
+     }
+      function handleSubmitForm(e){
+     //  e.preventDefault();
+     
+      console.log(e);
+      fetch("http://localhost:3000/categories", {
+        method: "POST",
+        headers: {
+           "Content-Type": "application/json"
+           
+         },
+         body: JSON.stringify(e) 
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log("submitted")
+        form.resetFields();
+      })
+
+      .catch(err => console.log(err.message))
+      }
 
   return (
     <div>    
-    <Form form={form} onFinish={handleSubmitForm} onSubmit={handleSubmitForm}> 
-    <Form.Item name='category'>
-       <input></input>
+    <Form form={form} {...layout} name="nest-messages" onFinish={handleSubmitForm} onSubmit={handleSubmitForm}> 
+    <Form.Item name='category_name' label='category'>
+       <input/>
     </Form.Item>
     <Form.Item>
        <Button type="primary" htmlType="submit">Submit</Button>
