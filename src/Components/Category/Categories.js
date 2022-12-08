7a60aea56b54a5fb21190984ac12fdeacce7c85c
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-08 10:35:42 +0300

    created the categories

---
+++ src/Components/Category/Categories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Categories = ({id,category_name}) => {
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{category_name}</td>
+      
+ 
+    </tr>
+
+  )
+}
+
+export default Categories
