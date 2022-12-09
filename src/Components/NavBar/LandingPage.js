6a13d1aeee0811a098d76008ea0a729b83f238c8
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-09 15:08:12 +0300

    changed routes

--- src/Components/NavBar/LandingPage.js
+++ src/Components/NavBar/LandingPage.js
@@ -11,12 +11,12 @@ const LandingPage = () => {
     <NavLink style={{
           padding: '0 24px 24px', float: 'Left'
         }} >Metro Help Desk</NavLink>
-      <NavLink to="/DashBoard" style={{
+      <NavLink to="/Login" style={{
           padding: '0 24px 24px',float: 'right'
         }}>Login</NavLink>
       
       
-      <NavLink to="/Login" style={{
+      <NavLink to="" style={{
           padding: '0 24px 24px', float: 'right'
         }}>Contact Us</NavLink> 
     </nav>
@@ -27,12 +27,12 @@ const LandingPage = () => {
         <h2>How can we help you?</h2>
         </div>
         <div className='buttons'>
-        <h3 className='signin'>Sign in for expedited support</h3>
+        <h4 className='signin'>Sign in for expedited support</h4>
         <Link to="/SignUp">
-        <button className='button'>Sign In</button>
+        <button className='button1'>Sign In</button>
         </Link>
         <Link to ="/Login">
-        <button className='button'>Sign up</button>
+        <button className='button2'>Sign up</button>
         </Link>
     </div>
     <div>
@@ -60,8 +60,8 @@ const LandingPage = () => {
     </div>
     <div>
         <footer>
-            <h4>Contact us at 071000000</h4>
-            <h4>Always ready to serve you</h4>
+            <h4 className='footer'>Contact us at 071000000</h4>
+            <h4 className='footer1' >Always ready to serve you</h4>
         </footer>
     </div>
     </div>
